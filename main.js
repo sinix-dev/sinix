7ccfd80ecc9e43ec5f2641601c7b81310fe1c9e4
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-03-25 00:19:43 +0530

    :heart_eyes: Wrapped with electron

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express');
 const { GraphQLServer, PubSub } = require('graphql-yoga')
+const { app, BrowserWindow } = require('electron')
 
 const Query = require('./src/query')
 const Mutation = require('./src/mutation')
@@ -11,7 +12,6 @@ const resolvers = {
   Mutation,
   Subscription
 }
-
 
 const pubsub = new PubSub()
 
@@ -29,4 +29,35 @@ const options = {
 }
 
 server.express.use("/", express.static(path.join(__dirname, 'public')))
-server.start(options, () => console.log('Server started.'))
+
+let server_pr = server.start(options)
+let mainWindow
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    },
+    fullscreen: true
+  })
+
+  server_pr.then(() => {
+    mainWindow.loadURL('http://localhost:4143')
+  })
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+})
+
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) createWindow()
+})
