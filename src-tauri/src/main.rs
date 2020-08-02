23d63e8e1d8dc2fe64445e962b1f5299ae9679f4
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-08-02 21:48:22 +0530

    cargo fmt

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,18 @@
   windows_subsystem = "windows"
 )]
 
+// Max Hue: 359
+// Chosen Sat: 100%
+// Chosen Lit: 25%
+
 use std::collections::HashMap;
 use std::convert::Infallible;
 use std::sync::Arc;
 use tokio::sync::{mpsc, RwLock};
 use warp::{ws::Message, Filter, Rejection};
 
-mod handler;
 mod cmd;
+mod handler;
 mod ws;
 
 type Result<T> = std::result::Result<T, Rejection>;
@@ -18,9 +22,9 @@ type Clients = Arc<RwLock<HashMap<String, Client>>>;
 
 #[derive(Debug, Clone)]
 pub struct Client {
-    pub user_id: usize,
-    pub topics: Vec<String>,
-    pub sender: Option<mpsc::UnboundedSender<std::result::Result<Message, warp::Error>>>,
+  pub user_id: usize,
+  pub topics: Vec<String>,
+  pub sender: Option<mpsc::UnboundedSender<std::result::Result<Message, warp::Error>>>,
 }
 
 #[tokio::main]
@@ -35,11 +39,13 @@ async fn main() {
     .and(warp::body::json())
     .and(with_clients(clients.clone()))
     .and_then(handler::register_handler)
-    .or(register
-      .and(warp::delete())
-      .and(warp::path::param())
-      .and(with_clients(clients.clone()))
-      .and_then(handler::unregister_handler));
+    .or(
+      register
+        .and(warp::delete())
+        .and(warp::path::param())
+        .and(with_clients(clients.clone()))
+        .and_then(handler::unregister_handler),
+    );
 
   let publish = warp::path!("publish")
     .and(warp::body::json())
@@ -58,33 +64,32 @@ async fn main() {
     .or(publish)
     .with(warp::cors().allow_any_origin());
 
-/*
-  tauri::AppBuilder::new()
-    .invoke_handler(|_webview, arg| {
-      use cmd::Cmd::*;
-      match serde_json::from_str(arg) {
-        Err(e) => {
-          Err(e.to_string())
-        }
-        Ok(command) => {
-          match command {
-            // definitions for your custom commands from Cmd here
-            MyCustomCommand { argument } => {
-              //  your command code
-              println!("{}", argument);
-            }
+  /*
+    tauri::AppBuilder::new()
+      .invoke_handler(|_webview, arg| {
+        use cmd::Cmd::*;
+        match serde_json::from_str(arg) {
+          Err(e) => {
+            Err(e.to_string())
           }
-          Ok(())
+          Ok(command) => {
+            match command {
+              // definitions for your custom commands from Cmd here
+              MyCustomCommand { argument } => {
+                //  your command code
+                println!("{}", argument);
+              }
+            }
+            Ok(())
+          }
         }
-      }
-    })
-    .build()
-    .run();
-*/
+      })
+      .build()
+      .run();
+  */
 
   warp::serve(routes).run(([127, 0, 0, 1], 41431)).await;
 }
-
 
 fn with_clients(clients: Clients) -> impl Filter<Extract = (Clients,), Error = Infallible> + Clone {
   warp::any().map(move || clients.clone())
