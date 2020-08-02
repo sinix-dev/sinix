ab54bbe5c48b12ce5e256a3b3d9d7da880271733
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-08-02 20:54:23 +0530

    WS running

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,62 @@
   windows_subsystem = "windows"
 )]
 
-mod cmd;
+use std::collections::HashMap;
+use std::convert::Infallible;
+use std::sync::Arc;
+use tokio::sync::{mpsc, RwLock};
+use warp::{ws::Message, Filter, Rejection};
 
-fn main() {
+mod handler;
+mod cmd;
+mod ws;
+
+type Result<T> = std::result::Result<T, Rejection>;
+type Clients = Arc<RwLock<HashMap<String, Client>>>;
+
+#[derive(Debug, Clone)]
+pub struct Client {
+    pub user_id: usize,
+    pub topics: Vec<String>,
+    pub sender: Option<mpsc::UnboundedSender<std::result::Result<Message, warp::Error>>>,
+}
+
+#[tokio::main]
+async fn main() {
+  let clients: Clients = Arc::new(RwLock::new(HashMap::new()));
+
+  let health_route = warp::path!("health").and_then(handler::health_handler);
+
+  let register = warp::path("register");
+  let register_routes = register
+    .and(warp::post())
+    .and(warp::body::json())
+    .and(with_clients(clients.clone()))
+    .and_then(handler::register_handler)
+    .or(register
+      .and(warp::delete())
+      .and(warp::path::param())
+      .and(with_clients(clients.clone()))
+      .and_then(handler::unregister_handler));
+
+  let publish = warp::path!("publish")
+    .and(warp::body::json())
+    .and(with_clients(clients.clone()))
+    .and_then(handler::publish_handler);
+
+  let ws_route = warp::path("ws")
+    .and(warp::ws())
+    .and(warp::path::param())
+    .and(with_clients(clients.clone()))
+    .and_then(handler::ws_handler);
+
+  let routes = health_route
+    .or(register_routes)
+    .or(ws_route)
+    .or(publish)
+    .with(warp::cors().allow_any_origin());
+
+/*
   tauri::AppBuilder::new()
     .invoke_handler(|_webview, arg| {
       use cmd::Cmd::*;
@@ -27,4 +80,12 @@ fn main() {
     })
     .build()
     .run();
+*/
+
+  warp::serve(routes).run(([127, 0, 0, 1], 41431)).await;
+}
+
+
+fn with_clients(clients: Clients) -> impl Filter<Extract = (Clients,), Error = Infallible> + Clone {
+  warp::any().map(move || clients.clone())
 }
