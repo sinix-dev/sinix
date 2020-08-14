e939afd63bbd9207c65c874cb9edf34fcd97cb21
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-08-14 16:10:11 +0530

    chore: Add basic layout

---
+++ components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="navbar px-3 items-center">
+    <div class="font-bold tracking-widest">
+      SIN<span style="color: crimson">I</span>X
+    </div>
+    <div class="flex-1"></div>
+    <div>
+      Settings
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.navbar {
+  height: 50px;
+  @apply flex w-full;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="layout">
+    <Navbar />
+    <div class="view">
+      <div class="sidebar"></div>
+      <div class="content">
+        <Nuxt />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+:root {
+  --text-black: #24292E;
+}
+
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url(../static/fonts/Roboto-Regular.ttf);
+}
+
+html, body, #__nuxt, #__layout {
+  color: var(--text-black);
+  font-family: 'Roboto', sans-serif;
+
+  @apply w-full h-full
+}
+
+.layout {
+  grid-template-rows: 50px auto;
+
+  @apply grid h-full;
+}
+
+.view {
+  grid-template-columns: 250px auto;
+
+  @apply grid;
+}
+</style>
