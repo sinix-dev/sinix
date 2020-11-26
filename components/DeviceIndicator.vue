ba6f6da14e69cb971cc8d42566672eef9f7f2992
Jahnavi Gupta <guptajhnv@gmail.com>
2020-11-26 20:00:40 +0530

    feat: display online devices/users

---
+++ components/DeviceIndicator.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="indicator-wrapper">
+    <div class="indicator">
+      {{ username[0].toUpperCase() }}
+    </div>
+    <span class="status" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.indicator-wrapper {
+  margin-top: 8px;
+}
+
+.indicator {
+  width: 40px;
+  height: 40px;
+  display: flex;
+  color: #4A4A4A;
+  font-size: 22px;
+  font-weight: bold;
+  border-radius: 50%;
+  background: #C4C4C4;
+  align-items: center;
+  justify-content: center;
+}
+
+.status {
+  right: 8px;
+  width: 12px;
+  height: 12px;
+  display: block;
+  margin-top: -12px;
+  position: absolute;
+  border-radius: 50%;
+  background: #2EA62C;
+}
+</style>
---
+++ components/Rightbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="rightbar-wrapper">
+    <DeviceIndicator username="sanket143" />
+  </div>
+</template>
+
+<style scoped>
+.rightbar-wrapper {
+  display: flex;
+  justify-content: center;
+}
+</style>
