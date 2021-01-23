f3b764753f2741aecfc753dd5525fe558895f725
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2021-01-23 19:33:41 +0530

    fix: install from computer

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="page-wrapper">
     <h3>Library</h3>
+    <span class="link" @click="browse">Install from Computer</span>
   </div>
 </template>
 
 <script>
+import { open } from "tauri/api/dialog"
+import { emit } from "tauri/api/event"
+
 export default {
-  name: "Index"
+  name: "Index",
+  methods: {
+    browse(){
+      open().then((file) => {
+        emit("sinix-install", file)
+        console.log(file)
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  }
 }
 </script>
 
