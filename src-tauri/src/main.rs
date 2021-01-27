7a54557f52c3e8fe246451d789a6ceb25ffbc528
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2021-01-27 23:36:46 +0530

    chore: udp server as plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,18 +3,16 @@
   windows_subsystem = "windows"
 )]
 
-use sinix::joystick;
 use sinix::root;
 use sinix::plugins;
 
 mod cmd;
 
 fn main() {
-  let udp_socket_server = joystick::UdpSocketServer::new();
   let sinix_root = root::SinixRoot::new();
-  let game_webview = plugins::GameWebview;
+  let game_webview = plugins::GameWebview::new();
+  let udp_socket_server = plugins::UdpSocketServer::new();
 
-  println!("main: {:?}", std::thread::current().id());
   tauri::AppBuilder::new()
     .plugin(udp_socket_server)
     .plugin(sinix_root)
--- src-tauri/src/plugins/mod.rs
+++ src-tauri/src/plugins/mod.rs
@@ -1,3 +1,5 @@
 mod game_webview;
+mod udp_server;
 
 pub use game_webview::GameWebview;
+pub use udp_server::UdpSocketServer;
