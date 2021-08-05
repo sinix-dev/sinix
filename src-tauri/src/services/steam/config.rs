76e72b8c5fdf97f796e8d0090d81142fbf1b4a73
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2021-08-06 00:01:51 +0530

    feat: add proper return type for search_in_steam_dirs

--- src-tauri/src/services/steam/config.rs
+++ src-tauri/src/services/steam/config.rs
@@ -9,20 +9,25 @@ static STEAM_DATA_DIRS: &'static [&str] = &[
     ".steam/debian-installation",
     "/usr/share/steam",
     "/usr/local/share/steam",
+    "Library/Application Support/Steam"
 ];
 
-fn search_in_steam_dirs(file: &str){
+fn search_in_steam_dirs(file: &str) -> Option<String> {
     if let Some(home_dir) = path::home_dir() {
         for candidate in STEAM_DATA_DIRS {
-            println!("{:?}", home_dir.join(candidate).canonicalize())
+            if let Ok(file_path) = home_dir.join(candidate).join(file).canonicalize() {
+                return Some(String::from(file_path.to_str().unwrap()));
+            }
         }
     }
+
+    return None;
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn scan_dirs() {
-        super::search_in_steam_dirs("bleh")
+        super::search_in_steam_dirs("config/loginusers.vdf");
     }
 }
