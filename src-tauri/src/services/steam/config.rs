994163ff4570456fc79da349674f620d9501e621
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2021-08-07 18:37:34 +0530

    chore: cargo fmt

--- src-tauri/src/services/steam/config.rs
+++ src-tauri/src/services/steam/config.rs
@@ -2,41 +2,41 @@ use std::fs;
 use tauri::api::path;
 
 static STEAM_DATA_DIRS: &'static [&str] = &[
-    ".steam",
-    ".local/share/steam",
-    ".local/share/Steam",
-    ".steam/steam",
-    ".var/app/com.valvesoftware.Steam/data/steam",
-    ".steam/debian-installation",
-    "/usr/share/steam",
-    "/usr/local/share/steam",
-    "Library/Application Support/Steam"
+  ".steam",
+  ".local/share/steam",
+  ".local/share/Steam",
+  ".steam/steam",
+  ".var/app/com.valvesoftware.Steam/data/steam",
+  ".steam/debian-installation",
+  "/usr/share/steam",
+  "/usr/local/share/steam",
+  "Library/Application Support/Steam",
 ];
 
 fn search_in_steam_dirs(file: &str) -> Option<String> {
-    if let Some(home_dir) = path::home_dir() {
-        for candidate in STEAM_DATA_DIRS {
-            if let Ok(file_path) = home_dir.join(candidate).join(file).canonicalize() {
-                return Some(String::from(file_path.to_str().unwrap()));
-            }
-        }
+  if let Some(home_dir) = path::home_dir() {
+    for candidate in STEAM_DATA_DIRS {
+      if let Ok(file_path) = home_dir.join(candidate).join(file).canonicalize() {
+        return Some(String::from(file_path.to_str().unwrap()));
+      }
     }
+  }
 
-    return None;
+  return None;
 }
 
 fn get_steam_user() -> super::util::SteamUser {
-    let filename = search_in_steam_dirs("config/loginusers.vdf").unwrap();
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+  let filename = search_in_steam_dirs("config/loginusers.vdf").unwrap();
+  let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
-    crate::services::steam::util::steam_vdf_parser(&contents)
+  crate::services::steam::util::steam_vdf_parser(&contents)
 }
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn scan_dirs() {
-        let steam_user = super::get_steam_user();
-        println!("{:?}", steam_user);
-    }
+  #[test]
+  fn scan_dirs() {
+    let steam_user = super::get_steam_user();
+    println!("{:?}", steam_user);
+  }
 }
--- src-tauri/src/services/steam/util.rs
+++ src-tauri/src/services/steam/util.rs
@@ -3,97 +3,96 @@ use regex::Regex;
 
 #[derive(Debug)]
 pub struct SteamUser {
-    steam_id: Option<String>,
-    account_name: Option<String>,
-    persona_name: Option<String>,
+  steam_id: Option<String>,
+  account_name: Option<String>,
+  persona_name: Option<String>,
 }
 
 impl SteamUser {
-    fn new() -> SteamUser {
-        SteamUser {
-            steam_id: None,
-            account_name: None,
-            persona_name: None,
-        }       
+  fn new() -> SteamUser {
+    SteamUser {
+      steam_id: None,
+      account_name: None,
+      persona_name: None,
     }
+  }
 }
 
 fn remove_end_quotes(text: &str) -> Option<regex::Captures> {
-    lazy_static! {
-        static ref re: Regex = Regex::new("\"(?P<token>.*)\"").unwrap();
-    }
+  lazy_static! {
+    static ref re: Regex = Regex::new("\"(?P<token>.*)\"").unwrap();
+  }
 
-    re.captures(text)
+  re.captures(text)
 }
 
 pub fn steam_vdf_parser(steam_vdf: &str) -> SteamUser {
-    #[derive(Debug,PartialEq)]
-    enum VDFToken {
-        START,
-        USER,
-        KEY,
-        VALUE,
-        END,
-    }
+  #[derive(Debug, PartialEq)]
+  enum VDFToken {
+    START,
+    USER,
+    KEY,
+    VALUE,
+    END,
+  }
 
-    let lines = steam_vdf.lines();
-    let mut state: VDFToken = VDFToken::START;
-    
-    let mut steam_user: SteamUser = SteamUser::new();
+  let lines = steam_vdf.lines();
+  let mut state: VDFToken = VDFToken::START;
 
-    for line in lines {
-        let line = line.trim();
-        let tokens = line.split_whitespace();
-        let mut key: String = String::new();
+  let mut steam_user: SteamUser = SteamUser::new();
 
-        for token in tokens {
-            if let Some(cap) = remove_end_quotes(token) {
-                let token = &cap["token"];
+  for line in lines {
+    let line = line.trim();
+    let tokens = line.split_whitespace();
+    let mut key: String = String::new();
 
-                match token {
-                    "{" => continue,
-                    "}" => {
-                        state = VDFToken::END;
-                    }
-                    "users" => {
-                        state = VDFToken::USER;
-                        continue;
-                    }
-                    _ => {
-                        // do nothing
-                    }
-                }
+    for token in tokens {
+      if let Some(cap) = remove_end_quotes(token) {
+        let token = &cap["token"];
 
-                match &state {
-                    VDFToken::START => {
-                        state = VDFToken::KEY;
-                    }
-                    VDFToken::USER => {
-                        steam_user.steam_id = Some(String::from(token));
-                        state = VDFToken::KEY;
-                    }
-                    VDFToken::KEY => {
-                        key = String::from(token);
-                        state = VDFToken::VALUE;
-                    }
-                    VDFToken::VALUE => {
-                        match key.as_str() {
-                            "AccountName" => steam_user.account_name = Some(String::from(token)),
-                            "PersonaName" => steam_user.persona_name = Some(String::from(token)),
-                            _ => {}
-                        }
-                        state = VDFToken::KEY;
-                    }
-                    VDFToken::END => break
-                }
+        match token {
+          "{" => continue,
+          "}" => {
+            state = VDFToken::END;
+          }
+          "users" => {
+            state = VDFToken::USER;
+            continue;
+          }
+          _ => {
+            // do nothing
+          }
+        }
+
+        match &state {
+          VDFToken::START => {
+            state = VDFToken::KEY;
+          }
+          VDFToken::USER => {
+            steam_user.steam_id = Some(String::from(token));
+            state = VDFToken::KEY;
+          }
+          VDFToken::KEY => {
+            key = String::from(token);
+            state = VDFToken::VALUE;
+          }
+          VDFToken::VALUE => {
+            match key.as_str() {
+              "AccountName" => steam_user.account_name = Some(String::from(token)),
+              "PersonaName" => steam_user.persona_name = Some(String::from(token)),
+              _ => {}
             }
+            state = VDFToken::KEY;
+          }
+          VDFToken::END => break,
         }
-
-        if state == VDFToken::END {
-            break;
-        }
-
+      }
     }
 
-    steam_user
+    if state == VDFToken::END {
+      break;
+    }
+  }
+
+  steam_user
 }
